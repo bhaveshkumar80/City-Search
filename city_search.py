7dbc7f148c27cb4b1bd459e98a15d3d5cdc755b3
Bhavesh Kumar <bhaveshkumar2806@gmail.com>
2022-01-31 11:34:59 +0530

    changed response

--- city_search.py
+++ city_search.py
@@ -8,20 +8,23 @@ def City_Search(loc_name):
     df = pd.read_csv('airport_codes.csv')
 
     location_data = []
-    for city, country, airport in zip(df['City name'], df['Country name'], df['Airport Name']):
-        if loc_name.lower() in city.lower():
-            location_data.append({'city': city, 'country': country, 'airport': airport})
+    try:
+        for city, country, airport in zip(df['City name'], df['Country name'], df['Airport Name']):
+            if loc_name.lower() in city.lower():
+                location_data.append({'city': city, 'country': country, 'airport': airport})
+            
+            if loc_name.lower() in country.lower():
+                location_data.append({'city': city, 'country': country, 'airport': airport})
+
+            if loc_name.lower() in airport.lower():
+                location_data.append({'city': city, 'country': country, 'airport': airport})
         
-        if loc_name.lower() in country.lower():
-            location_data.append({'city': city, 'country': country, 'airport': airport})
+            if len(location_data) >= 20:
+                break
 
-        if loc_name.lower() in airport.lower():
-            location_data.append({'city': city, 'country': country, 'airport': airport})
-    
-        if len(location_data) >= 20:
-            break
-
-    return location_data
+        return {'status': True, 'data': location_data}
+    except:
+        return {'status': False, 'data': []}
 
 # loc_name = "Delhi"
 # data = City_Search(loc_name)
